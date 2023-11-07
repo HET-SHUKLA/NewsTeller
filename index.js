4995aa980df545eca87997750ed65ce5d214c08c
HET SHUKLA <SHUKLAHET2704@gmailc.com>
2023-11-07 18:48:33 +0530

    Take data, just for testing

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ import {API_KEY} from "./secret.js";
 const app = express();
 const port = 3000;
 
+const LINK = "https://newsapi.org/v2/";
+
+const HEADER = {
+    headers :
+    {
+        "x-api-key": API_KEY,
+    }
+}
+
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+
+    const response = await axios.get(`${LINK}/top-headlines?country=us`, HEADER);
     res.render("index.ejs");
-    console.log(API_KEY);
 })
 
 app.listen(port,() => {
